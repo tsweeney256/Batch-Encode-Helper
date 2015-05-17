5e23b71e960b15f38e47821b3eeedd7b57afdf44
tsweeney256 <tsweeney256@gmail.com>
2015-05-16 21:45:41 -0400

    Initial creation of project

---
+++ Encoder-Helper-GUI/FormMain.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Encoder_Helper_GUI
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListBoxFiles = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // ListBoxFiles
+            // 
+            this.ListBoxFiles.AllowDrop = true;
+            this.ListBoxFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ListBoxFiles.FormattingEnabled = true;
+            this.ListBoxFiles.Location = new System.Drawing.Point(12, 12);
+            this.ListBoxFiles.Name = "ListBoxFiles";
+            this.ListBoxFiles.Size = new System.Drawing.Size(599, 368);
+            this.ListBoxFiles.TabIndex = 0;
+            this.ListBoxFiles.DragEnter += new System.Windows.Forms.DragEventHandler(this.OnDragEnter);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 390);
+            this.Controls.Add(this.ListBoxFiles);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ListBoxFiles;
+    }
+}
+
---
+++ Encoder-Helper-GUI/FormMain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encoder_Helper_GUI
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void OnDragEnter(object sender, DragEventArgs e)
+        {
+
+        }
+    }
+}
