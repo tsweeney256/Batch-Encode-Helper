a793534a604f6dfdcc398bc5ec1e858a0a423179
tsweeney256 <tsweeney256@gmail.com>
2015-05-28 02:14:14 -0400

    added in copyright notices

--- Encoder-Helper-GUI/Form_Settings.Designer.cs
+++ Encoder-Helper-GUI/Form_Settings.Designer.cs
@@ -1,4 +1,21 @@
-﻿namespace Encoder_Helper_GUI
+﻿/*Batch Encoder Helper
+Copyright (C) 2015 Thomas Sweeney
+
+This file is part of Batch Encoder Helper.
+Batch Encoder Helper is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Batch Encoder Helper is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+
+namespace Encoder_Helper_GUI
 {
     partial class Form_Settings
     {
